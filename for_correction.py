dc66bd12bf0784f5d58113e05d25d3e3d3181737
trufi <yura@gmail.com>
2024-11-29 14:22:04 +0200

    homework

---
+++ for_correction.py
@@ -0,0 +1,11 @@
+# expected result in the form: My name is David, I am 14 years old👣
+
+# example
+# f = '\N{Footprints}'  # not informative naming, the correct code below
+smile_footprint = '\U0001F463'
+
+user_name = "Max"
+user_age = "15"
+result = "My name is " + user_name + ", I am " + user_age + " years old " + smile_footprint
+
+print(result)
