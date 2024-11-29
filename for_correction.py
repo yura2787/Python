a4440b233dc821c82fe5d41774c1434b3dfab37e
trufi <yura@gmail.com>
2024-11-29 14:25:00 +0200

    homework

--- for_correction.py
+++ for_correction.py
@@ -4,8 +4,8 @@
 # f = '\N{Footprints}'  # not informative naming, the correct code below
 smile_footprint = '\U0001F463'
 
-user_name = "Max"
-user_age = "15"
+user_name = 'Max'
+user_age = '15'
 result = "My name is " + user_name + ", I am " + user_age + " years old " + smile_footprint
 
 print(result)
