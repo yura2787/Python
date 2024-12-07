04f963bd03b9a972d03c901ca3c6d2c5ded385a6
trufi <yura@gmail.com>
2024-12-07 21:02:41 +0200

    homework

---
+++ homework.py
@@ -0,0 +1,18 @@
+from pywebio.input import input as pw_input, NUMBER
+from pywebio.output import put_text
+
+price = 0
+
+age = pw_input(label='Введіть свій вік', type=NUMBER)
+if age < 6:
+    price = 0
+elif age >= 6 and age <= 12:
+    price = 50
+elif age >= 13 and age <= 17:
+    price = 75
+elif age >= 18 and age <= 60:
+    price = 100
+elif age > 61:
+    price = 70
+
+put_text(f'Вхід у зоопарк коштує {price} грн')
